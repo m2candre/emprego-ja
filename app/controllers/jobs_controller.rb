96c817994dd5c54e2e2cade77afbc4b671b103a9
Marcos e Ariadyne <m2c.andre@gmail.com>
2016-07-11 20:25:49 -0300

    Usuário edita vaga

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,21 +5,40 @@ class JobsController < ApplicationController
 
   def new
     @job = Job.new
-
     @companies = Company.all
   end
 
   def create
-    @job = Job.create(params.require(:job).permit(:title, :location,:category, :company_id, :description, :featured))
-
-    if @job.persisted?
+    @job = Job.new(job_params)
+    if @job.save
       redirect_to @job
     else
       flash[:notice] = "Não foi possível criar a vaga"
       @companies = Company.all
-
       render :new
     end
-
   end
+
+  def edit
+    @job = Job.find(params[:id])
+    @companies = Company.all
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update_attributes(job_params)
+      redirect_to @job
+    else
+      flash[:notice] = "Não foi possível atualizar a vaga"
+      @companies = Company.all
+      render :edit
+    end
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :location,:category, :company_id, :description, :featured)
+  end
+
 end
