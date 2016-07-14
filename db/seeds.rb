9b8f354c8f30ce6b2baf4adbb59f4d9f4dfc1b78
Marcos e Ariadyne <m2c.andre@gmail.com>
2016-07-14 19:29:25 -0300

    Destacar empresas com muitas vagas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+compCampus = Company.create(name: 'Campus Code', location: 'São Paulo', mail: 'contato@campus.com.br' , phone: '2369-3476')
+compLoca   = Company.create(name: 'Locaweb'    , location: 'São Paulo', mail: 'contato@locaweb.com.br', phone: '3333-3333')
+
+catDev = Category.create(name: 'Desenvolvedor')
+catFrt = Category.create(name: 'Front-End')
+catSys = Category.create(name: 'SysAdmin')
+catSup = Category.create(name: 'Suporte')
+
+Category.create(name: 'Programador')
+Category.create(name: 'Back-End')
+
+
+Job.create(title: 'Dev Master', category: catDev, company: compCampus, location: 'Rio de Janeiro', description: 'Vaga para Dev Master para Bootcamp Rails')
+Job.create(title: 'SysAdmin'  , category: catSys, company: compCampus, location: 'Rio de Janeiro', description: 'Administrador de sistema')
+Job.create(title: 'Suporte'   , category: catSup, company: compLoca  , location: 'São Paulo'     , description: 'Atendimento ao cliente')
+Job.create(title: 'Web Dev'   , category: catFrt, company: compLoca  , location: 'São Paulo'     , description: 'Vaga para Desenvolvedor Web')
